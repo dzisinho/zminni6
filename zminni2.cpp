ff50e6977ac25e12a2759631bda448446946d297
dzisinho <dzisbogdan26@gmail.com>
2024-11-25 21:39:14 +0200

    completed

--- zminni2.cpp
+++ zminni2.cpp
@@ -1,2 +1,18 @@
 
+#include <iostream>
+using namespace std;
+int main() {
+    int sec;
+    cin >> sec;
 
+    int hrLeft = (8 * 3600 - sec) / 3600;
+
+    if (hrLeft < 0) {
+        cout << "Workday is finish\n";
+    }
+    else {
+        cout << hrLeft << " full hours left\n";
+    }
+
+    return 0;
+}
