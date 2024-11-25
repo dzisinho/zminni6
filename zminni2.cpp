1823a251c454d8075579862a2c9acca06c7fcace
dzisinho <dzisbogdan26@gmail.com>
2024-11-25 21:37:38 +0200

    completed

--- zminni2.cpp
+++ zminni2.cpp
@@ -1,2 +1,25 @@
 
 
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int sec;
+    cout << "Enter the number of seconds since the start of the day: ";
+    cin >> sec;
+
+    int hr = sec / 3600;
+    int min = (sec % 3600) / 60;
+    int remainingSec = sec % 60;
+
+    int secLeft = 86400 - sec; 
+    int hrLeft = secLeft / 3600;
+    int minLeft = (secLeft % 3600) / 60;
+    int secLeftFinal = secLeft % 60;
+
+    cout << "Current time: " << hr << " hr " << min << " min " << remainingSec << " sec\n";
+    cout << "midnight: " << hrLeft << " hr " << minLeft << " min " << secLeftFinal << " sec\n";
+
+    return 0;
+}
